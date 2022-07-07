6d25d96635886b1dd391ec28b16d8a17dd083650
Nicholas Pwi <nicholaspwi@live.com>
2022-07-07 19:48:55 +0800

    Signed-off-by: Nicholas Pwi <nicholaspwi@live.com>

---
+++ CompleteLevel.py
@@ -0,0 +1,3 @@
+#Complete Level script will take in the generated level and complete it
+
+#Upon completion, analysis of how many steps and attempts must be recorded and returned back to main as storage for main analysis
---
+++ Menu.py
@@ -0,0 +1,10 @@
+#Main Menu will contain 4 buttons
+#Each button will trigger a function as described below
+
+#Button1 Generate Level
+
+#Button2 Increase Difficulty
+
+#Button3 Decrease Difficulty
+
+#Button4 Show Analysis
